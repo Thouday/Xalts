b4984e3a2eef2c3c18f1703a666429dd4df1708a
Pavan <thouday.pavan99@gmail.com>
2025-03-07 09:33:04 +0530

    Add files via upload
    commit

---
+++ Main.java
@@ -0,0 +1,91 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Main {
+
+   private WebDriver driver;
+   private WebDriverWait wait;
+   private static WebElements webElements;
+
+   @BeforeTest
+   public void beforeclass(){
+      webElements = new WebElements();
+   }
+
+   @BeforeClass
+   public void before(){
+      driver = new ChromeDriver();
+      driver.manage().window().maximize();
+      driver.get("https://xaltsocnportal.web.app/");
+      wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+      PageFactory.initElements(driver,this);
+      PageFactory.initElements(driver, webElements);
+   }
+
+   public void Credentials(){
+      webElements.getCredentialsFields().getFirst().sendKeys("thouday.pavan99@gmail.com");
+      webElements.getCredentialsFields().get(1).sendKeys("Pavan@24");
+   }
+
+   @Test
+   public void test() throws Exception {
+      webElements.getGetStarted().click();
+      webElements.getAlreadyAccount().click();
+      Thread.sleep(1000);
+      Credentials();
+      webElements.getSignIn().click();
+      Thread.sleep(2000);
+      webElements.getGetStarted().click();
+      Thread.sleep(1000);
+      webElements.getOnboard().click();
+      for (int i = 0; i < 3; i++) {
+         Thread.sleep(2000);
+         webElements.getOCNNodeIDCredentials().getFirst().sendKeys("NodeID-" + i);
+         webElements.getOCNNodeIDCredentials().get(1).sendKeys("193.16.1." + i);
+         webElements.getAddNode().click();
+      }
+      for (int i = 0; i < 2; i++) {
+         webElements.getNext().click();
+         Thread.sleep(1000);
+      }
+      webElements.getSubmit().click();
+   }
+
+   @Test
+   public void test1() throws Exception{
+      webElements.getGetStarted().click();
+      webElements.getAlreadyAccount().click();
+      Thread.sleep(1000);
+      Credentials();
+      webElements.getSignIn().click();
+      Thread.sleep(2000);
+      webElements.getGetStarted().click();
+      Thread.sleep(1000);
+      webElements.getLaunchOCN().click();
+      for(int i=0; i<3; i++){
+         Thread.sleep(1000);
+         webElements.getOCNNodeIDCredentials().getFirst().sendKeys("Pavan");
+
+      }
+   }
+
+   @AfterMethod
+   public void quit(){
+      if(driver!=null){
+//         driver.quit();
+      }
+   }
+
+}
---
+++ WebElements.java
@@ -0,0 +1,86 @@
+package org.example;
+
+import org.checkerframework.checker.units.qual.N;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+
+import java.util.List;
+
+public class WebElements {
+
+    @FindBy(xpath = "//button[text()='Get Started']")
+    private WebElement GetStarted;
+
+    @FindBy(xpath = "//button[text()='Already have an account? Click here to sign in.']")
+    private WebElement AlreadyAccount;
+
+    @FindBys({
+            @FindBy(id = "outlined-basic")
+    })
+    private List<WebElement> CredentialsFields;
+
+    @FindBy(xpath = "//button[text()='Sign In']")
+    private WebElement SignIn;
+
+    @FindBy(xpath = "//h2[text()='Onboard OCN Node']")
+    private WebElement Onboard;
+
+    @FindBys({
+            @FindBy(xpath = "//div[contains(@class,'MuiInputBase-root')]/input")
+    })
+    private List<WebElement> OCNNodeIDCredentials;
+
+    @FindBy(xpath = "//button[text()='+ Add Node ']")
+    private WebElement AddNode;
+
+    @FindBy(xpath = "//button[text()='Next']")
+    private WebElement Next;
+
+    @FindBy(xpath = "//button[text()='Submit']")
+    private WebElement Submit;
+
+    @FindBy(xpath = "//h2[text()='Launch OCN Child Network']")
+    private WebElement LaunchOCN;
+
+    public WebElement getGetStarted() {
+        return GetStarted;
+    }
+
+    public WebElement getAlreadyAccount() {
+        return AlreadyAccount;
+    }
+
+    public List<WebElement> getCredentialsFields() {
+        return CredentialsFields;
+    }
+
+    public WebElement getSignIn() {
+        return SignIn;
+    }
+
+    public WebElement getOnboard() {
+        return Onboard;
+    }
+
+    public List<WebElement> getOCNNodeIDCredentials(){
+        return OCNNodeIDCredentials;
+    }
+
+    public WebElement getAddNode() {
+        return AddNode;
+    }
+
+    public WebElement getNext(){
+        return Next;
+    }
+
+    public WebElement getSubmit(){
+        return Submit;
+    }
+
+    public WebElement getLaunchOCN(){
+        return LaunchOCN;
+    }
+
+}
